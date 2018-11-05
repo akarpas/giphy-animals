6088fa2b7e1dac78ef36ada6bec1b2eb106ae89e
Andreas Karpasitis <karpasitis@outlook.com>
2018-11-05 22:54:43 +0100

    add Provider and connect store to app

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
+import createStore from './store';
 import Home from './components/pages/Home';
 
 import style from './index.scss';
 
+const store = createStore({});
+
 const Application = () => {
   ReactDOM.render(
     <div className={style.appContainer}>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={Home} />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Home} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     </div>,
     document.getElementById('app'),
   );
