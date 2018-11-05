8d581b812d5a6a07a9295ac23ed01f66ee98bd03
Andreas Karpasitis <karpasitis@outlook.com>
2018-11-05 22:45:46 +0100

    implement animal buttons in home and connect router for links

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import Layout from '../../organisms/Layout';
 import style from './index.scss';
 
-const Home = () => (
-  <Layout>
-    <div className={style.content}>
-      <div className={style.title}>Choose your favorite animal:</div>
-    </div>
-  </Layout>
-);
+const Home = () => {
+  const animals = ['cat', 'dog', 'lion', 'monkey', 'elephant'];
+  return (
+    <Layout>
+      <div className={style.content}>
+        <div className={style.title}>Choose your favorite animal:</div>
+        <div className={style.animals}>
+          {animals.map(animal => (
+            <Link className={style.link} key={`link-${animal}`} to={{ pathname: '/animal', query: { animal } }}>
+              <button className={style.button} id={animal} key={animal} type="button">{animal}</button>
+            </Link>))}
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-export default Home;
+export default withRouter(Home);
