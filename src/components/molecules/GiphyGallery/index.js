8e68cd130e9b21c25b860915e055e8bc66962d43
Andreas Karpasitis <karpasitis@outlook.com>
2018-11-07 19:21:05 +0100

    add link to GiphyGallery to link to GiphyModal on click and view giphy

--- src/components/molecules/GiphyGallery/index.js
+++ src/components/molecules/GiphyGallery/index.js
@@ -1,24 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import style from './index.scss';
 
 const GiphyGallery = (props) => {
-  const { giphies } = props;
+  const { giphies, allGiphies } = props;
 
   return (
     <div className={style.gallery}>
-      {giphies.map((giphy) => {
+      {giphies.map((giphy, index) => {
         const key = giphy.url.split('media/')[1];
         return (
-          <div key={`${key}-container`} className={style.gifContainer}>
+          <Link
+            to={{ pathname: `/animal/${index}`, state: { giphies: allGiphies, index } }}
+            key={`${key}-container`}
+            className={style.gifContainer}
+          >
             <img
               key={key}
               src={giphy.url}
               alt={giphy.title}
-              className={style.gifGallery}
+              className={style.gif}
               height="20vw"
             />
-          </div>
+          </Link>
         );
       })}
     </div>
